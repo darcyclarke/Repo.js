393a4c2f2efa08b3a2e731edec85b549202d9f7d
dchusovitin <dchusovitin@gmail.com>
2012-07-03 18:38:57 +0400

    refactor

--- repo.js
+++ repo.js
@@ -208,17 +208,18 @@
                                 data: {},
                                 dataType: 'jsonp',
                                 success: function(response){
-                                    var extension = response.data.name.split('.').pop().toLowerCase(),
+                                    var fileContainer = $('<div class="file page" id="' + link.data('id') + '"></div>'),
+                                        extension = response.data.name.split('.').pop().toLowerCase(),
                                         mimeType = getMimeTypeByExtension(extension);
                                         
                                     if('image' === mimeType.split('/').shift()){
-                                        el = $('<div class="file page" id="' + link.data('id') + '"><div class="image"><span class="border-wrap"><img src="" /></span></div></div>').appendTo(repo);
+                                        el = fileContainer.append($('<div class="image"><span class="border-wrap"><img src="" /></span></div>')).appendTo(repo);
                                         el.find('img')
                                             .attr('src', 'data:' + mimeType + ';base64,' + response.data.content)
                                             .attr('alt', response.data.name);
                                     }
                                     else {
-                                        el = $('<div class="file page" id="' + link.data('id') + '"><pre><code></code></pre></div>').appendTo(repo);
+                                        el = fileContainer.append($('<pre><code></code></pre>')).appendTo(repo);
                                         if(typeof _this.extensions[extension] != 'undefined')
                                             file.find('code').addClass(_this.extensions[extension]);
                                         el.find('code').html(String(decode64(response.data.content)).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;'));
