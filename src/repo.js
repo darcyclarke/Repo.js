c299ddc3bb44e0877c8fee37ab218c1fbf5101f3
Rich Gilbank <richgilbank@gmail.com>
2014-09-03 19:26:11 -0400

    Fix indentation

--- src/repo.js
+++ src/repo.js
@@ -12,59 +12,61 @@
   $.fn.repo = function( options ){
 
     // Context and Base64 methods
-    var _this   = this,
-      keyStr64  = "ABCDEFGHIJKLMNOP" + "QRSTUVWXYZabcdef" + "ghijklmnopqrstuv" + "wxyz0123456789+/" + "=",
-      encode64  = function(a){a=escape(a);var b="";var c,d,e="";var f,g,h,i="";var j=0;do{c=a.charCodeAt(j++);d=a.charCodeAt(j++);e=a.charCodeAt(j++);f=c>>2;g=(c&3)<<4|d>>4;h=(d&15)<<2|e>>6;i=e&63;if(isNaN(d)){h=i=64}else if(isNaN(e)){i=64}b=b+keyStr64.charAt(f)+keyStr64.charAt(g)+keyStr64.charAt(h)+keyStr64.charAt(i);c=d=e="";f=g=h=i=""}while(j<a.length);return b},
-      decode64  = function(a){var b="";var c,d,e="";var f,g,h,i="";var j=0;var k=/[^A-Za-z0-9\+\/\=]/g;if(k.exec(a)){}a=a.replace(/[^A-Za-z0-9\+\/\=]/g,"");do{f=keyStr64.indexOf(a.charAt(j++));g=keyStr64.indexOf(a.charAt(j++));h=keyStr64.indexOf(a.charAt(j++));i=keyStr64.indexOf(a.charAt(j++));c=f<<2|g>>4;d=(g&15)<<4|h>>2;e=(h&3)<<6|i;b=b+String.fromCharCode(c);if(h!=64){b=b+String.fromCharCode(d)}if(i!=64){b=b+String.fromCharCode(e)}c=d=e="";f=g=h=i=""}while(j<a.length);return unescape(b)},
-      transition  = function(el, direction, init){
-        var opposite  = (direction === 'left') ? '' : 'left';
+    var _this   = this;
+    var keyStr64  = "ABCDEFGHIJKLMNOP" + "QRSTUVWXYZabcdef" + "ghijklmnopqrstuv" + "wxyz0123456789+/" + "=";
+    var encode64  = function(a){a=escape(a);var b="";var c,d,e="";var f,g,h,i="";var j=0;do{c=a.charCodeAt(j++);d=a.charCodeAt(j++);e=a.charCodeAt(j++);f=c>>2;g=(c&3)<<4|d>>4;h=(d&15)<<2|e>>6;i=e&63;if(isNaN(d)){h=i=64}else if(isNaN(e)){i=64}b=b+keyStr64.charAt(f)+keyStr64.charAt(g)+keyStr64.charAt(h)+keyStr64.charAt(i);c=d=e="";f=g=h=i=""}while(j<a.length);return b};
+    var decode64  = function(a){var b="";var c,d,e="";var f,g,h,i="";var j=0;var k=/[^A-Za-z0-9\+\/\=]/g;if(k.exec(a)){}a=a.replace(/[^A-Za-z0-9\+\/\=]/g,"");do{f=keyStr64.indexOf(a.charAt(j++));g=keyStr64.indexOf(a.charAt(j++));h=keyStr64.indexOf(a.charAt(j++));i=keyStr64.indexOf(a.charAt(j++));c=f<<2|g>>4;d=(g&15)<<4|h>>2;e=(h&3)<<6|i;b=b+String.fromCharCode(c);if(h!=64){b=b+String.fromCharCode(d)}if(i!=64){b=b+String.fromCharCode(e)}c=d=e="";f=g=h=i=""}while(j<a.length);return unescape(b)};
 
-        if(init){
-          el.addClass('active');
-          _this.container.css({'height' : calculateHeight(el) + 'px'});
-        } else {
-          _this.container
-            .find('.page.active')
-            .css('position','absolute')
-            .addClass(direction)
-            .removeClass('active')
-            .end()
-            .css({'height' : calculateHeight(el) + 'px'});
-          el.addClass('active')
-            .removeClass(opposite)
-            .delay(250)
-            .queue(function(){
-              $(this).css('position','relative').dequeue();
-            });
-        }
-      },
+    var transition  = function(el, direction, init) {
+      var opposite  = (direction === 'left') ? '' : 'left';
 
-      calculateHeight = function(el){
-        // This calculates the height of the bounding box for the repo display.
-        // clientHeight is element containing fetched results, plus the h1 tag, plus
-        // the div repo margin has of 15 pixels.
-        return (el[0].clientHeight + _this.container.find('h1').outerHeight(true) + 15);
-      },
+      if(init){
+        el.addClass('active');
+        _this.container.css({'height' : calculateHeight(el) + 'px'});
+      } else {
+        _this.container
+          .find('.page.active')
+          .css('position','absolute')
+          .addClass(direction)
+          .removeClass('active')
+          .end()
+          .css({'height' : calculateHeight(el) + 'px'});
+        el.addClass('active')
+          .removeClass(opposite)
+          .delay(250)
+          .queue(function(){
+            $(this).css('position','relative').dequeue();
+          });
+      }
+    },
 
-      getMimeTypeByExtension = function(extension){
-        var mimeTypes = {
-          // images
-          'png': 'image/png',
-          'gif': 'image/gif',
-          'jpg': 'image/jpeg',
-          'jpeg': 'image/jpeg',
-          'ico': 'image/x-icon'
-        };
-        return mimeTypes[extension] ? mimeTypes[extension] : 'text/plain';
+    calculateHeight = function(el){
+      // This calculates the height of the bounding box for the repo display.
+      // clientHeight is element containing fetched results, plus the h1 tag, plus
+      // the div repo margin has of 15 pixels.
+      return (el[0].clientHeight + _this.container.find('h1').outerHeight(true) + 15);
+    },
+
+    getMimeTypeByExtension = function(extension){
+      var mimeTypes = {
+        // images
+        'png': 'image/png',
+        'gif': 'image/gif',
+        'jpg': 'image/jpeg',
+        'jpeg': 'image/jpeg',
+        'ico': 'image/x-icon'
       };
+      return mimeTypes[extension] ? mimeTypes[extension] : 'text/plain';
+    };
 
     // Settings
     _this.settings = $.extend({
-        user  : '',
-        name  : '',
-        branch  : 'master',
-        css   : '{{{ css }}}'
-      }, options);
+      user  : '',
+      name  : '',
+      branch: 'master',
+      file  : false,
+      css   : '{{{ css }}}'
+    }, options);
 
     // Extension Hashes
     _this.extensions = {
@@ -72,6 +74,7 @@
       css     : 'css',
       html    : 'markup',
       js      : 'javascript',
+      json    : 'javascript',
       md      : 'markdown',
       php     : 'php',
       py      : 'python',
