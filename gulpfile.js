fe36dcb8d2918b4e46dce051a01ddf17bb822db3
Darcy Clarke <darcy@darcyclarke.me>
2015-11-19 14:36:51 -0500

    Initialize clean up

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+
+// ----------------------------
+// Requirements
+// ----------------------------
+
+var gulp    = require('gulp');
+var plugins = require('gulp-load-plugins');
+
+var license = """
+/*!
+ * Repo.js
+ * @author Darcy Clarke
+ * Copyright (c) 2015 Darcy Clarke
+ * Dual licensed under the MIT and GPL licenses.
+ * http://darcyclarke.me/
+ */
+"""
+
+// ----------------------------
+// Paths
+// ----------------------------
+
+var paths = {
+  main: './src/repo.js',
+  vendor: './src/vendor/**/*.js',
+  styles: './src/styles/**/*.styl'
+  dist: './dist/'
+};
+
+// ----------------------------
+// Build Task
+// ----------------------------
+
+gulp.task( 'build', function () {
+
+});
